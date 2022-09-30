de47335c7eeb38f8fd919f6e94105a3e0394a1df
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-30 13:42:57 +0530

    [Subodh]Test Enter the valid eight digit password

---
+++ Rule1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationMstest
+{
+    public class Rule1
+    {
+        public string password;
+
+        public Rule1(string password)
+        {
+            this.password = password;
+        }
+
+        public string ValidatePassword()
+        {
+            var hasLowerChar = new Regex("^(?=.*[a-z]).{8,}$");
+            if (!hasLowerChar.IsMatch(password))
+            {
+                Console.WriteLine("password should have minimum 8 characters");
+                return null;
+            }
+            else
+            {
+
+                return password;
+            }
+        }
+    }
+}
